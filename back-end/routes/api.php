13042a43fb6d92082cb926573c2d4fb7982f4ed5
mitocon <workinfo.mkt@gmail.com>
2024-11-21 20:55:03 +0900

    ルーティングのリネーム

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\SecuritiesReportController;
 
-Route::get('/get_securities_report', [SecuritiesReportController::class, 'getXBRLFromEdinet']);
+Route::get('/get_xbrl_from_edinet', [SecuritiesReportController::class, 'getXBRLFromEdinet']);
 
 Route::get('/healthcheck', function () {
     return response()->json(['status' => 'OK']);
